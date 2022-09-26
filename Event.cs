bf90dc0d9af53dfaef3b9a619c557f3e49883d3b
agostinopique <agostino.pique@gmail.com>
2022-09-26 14:57:16 +0200

    Completato milestone 1; Aggiunte funzioni richieste per i controlli e le stampe dei vari dati;

--- Event.cs
+++ Event.cs
@@ -14,7 +14,7 @@ public class Event
         this.maxCapacity = CheckCapacity(maxCapacity);
         this.bookedSeats = 0;
     }
-
+    #region DataControls
     public DateTime CheckDate(DateTime date)
     {
         DateTime today = DateTime.Today;
@@ -43,5 +43,52 @@ public class Event
             throw new Exception();
         }
         return maxCapacity;
+    }
+    #endregion DataControls 
+
+
+    //aggiunge i posti passati come parametro ai posti prenotati. Se l’evento è già passato o non ha posti o non ha più posti disponibili deve sollevare un’eccezione.
+    public void BookSeat(int bookedSeats)
+    {
+        DateTime today = DateTime.Today;
+        DateTime date = this.date;
+        int result = DateTime.Compare(today, date);
+        if (result >= 0)
+        {
+            throw new Exception("L'evento é gia passato");
+        }
+        else if(this.maxCapacity == 0)
+        {
+            throw new Exception("Non ci sono posti disponibili per questo evento");
+        } 
+        else if(this.bookedSeats == this.maxCapacity)
+        {
+            throw new Exception("Non ci sono abbastanza posti disponibili");
+        }
+        this.bookedSeats += bookedSeats;
+    }
+
+    //riduce i posti prenotati del numero di posti indicati come parametro.Se l’evento è già passato o non ci sono i posti da disdire sufficienti, deve sollevare un’eccezione.
+    public void FreeSeats(int seats)
+    {
+        DateTime today = DateTime.Today;
+        DateTime date = this.date;
+        int result = DateTime.Compare(today, date);
+        if (result >= 0)
+        {
+            throw new Exception("L'evento é gia passato");
+        }
+        else if (seats > this.bookedSeats)
+        {
+            throw new Exception("Stai cercando di disdire piú posti di quelli prenotati!");
+        }
+        this.bookedSeats -= seats;
+    }
+
+    public string FormatDateTitle()
+    {
+        string formatDate = this.date.ToString("dd/MM/yyyy");
+        string formatDateTitle = $"{formatDate} - {this.title}";
+        return formatDateTitle; 
     }
 }
